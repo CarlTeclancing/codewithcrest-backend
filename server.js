ed874e29ed839cc731289e3dfe2d37b0d4236bc7
Domguia-Simo <domguiasimoulrich@gmail.com>
2025-09-03 13:09:48 +0200

    fixed issue by removing a route

--- server.js
+++ server.js
@@ -48,9 +48,6 @@ app.get('/', (req, res) => {
     res.send("Welcome to codewithcrest")
 });
 
-app.get("*" ,(req ,res)=>{
-    return res.status(404).send('NOT FOUND')
-})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
